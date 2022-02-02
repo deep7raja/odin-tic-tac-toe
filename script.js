27c7c2a00b2912d81bd8430f12b3881ebf63463e
deep7raja <56893558+deep7raja@users.noreply.github.com>
2022-02-02 15:02:22 +0530

    concept of two player, winner added

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let cell = document.querySelector('.cell');
+let victoryDiv = document.querySelector('#victory-div');
+let currentPlayerDiv = document.querySelector('#currentPlayerDiv');
 
-console.log(cell.dataset.type);
-
-
-const boardManager = ()=>{
+const BoardManager = ()=>{
     let boardList = ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'];
     // ['cross', 'circle', 'none',
     // 'none' , 'cross' , 'circle',
@@ -11,10 +9,12 @@ const boardManager = ()=>{
 
     const reset = function(){
         boardList = ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'];
+        victoryDiv.textContent = '';
     }
 
     const draw = function(){
         const boardChildren = document.querySelector('.board').children;
+        currentPlayerDiv.textContent = `Current Player: ${currentPlayer.name}`;
         for(let i=0; i<boardChildren.length; ++i){
             if(boardList[i] !== 'none'){
                 boardChildren[i].querySelector('img').src = `${boardList[i]}.svg`;
@@ -27,12 +27,14 @@ const boardManager = ()=>{
                 const divCircle = boardChildren[i].querySelector('.btn-circle');
                 divCross.onclick = ()=>{
                     boardList[i] = 'cross';
-                    console.log(validate());
+                    validate();
+                    currentPlayer.swap();
                     draw();
                 };
                 divCircle.onclick = ()=>{
                     boardList[i] = 'circle';
-                    console.log(validate());
+                    validate();
+                    currentPlayer.swap();
                     draw();
                 }; 
             }
@@ -44,20 +46,39 @@ const boardManager = ()=>{
     // 6 7 8
 
     const validate = function(){
-        return ((boardList[0] === boardList[1] && boardList[1] === boardList[2] && boardList[2] !== 'none') ||
+        if((boardList[0] === boardList[1] && boardList[1] === boardList[2] && boardList[2] !== 'none') ||
                 (boardList[3] === boardList[4] && boardList[4] === boardList[5] && boardList[5] !== 'none') ||
                 (boardList[6] === boardList[7] && boardList[7] === boardList[8] && boardList[8] !== 'none') ||
                 (boardList[0] === boardList[3] && boardList[3] === boardList[6] && boardList[6] !== 'none') ||
                 (boardList[1] === boardList[4] && boardList[4] === boardList[7] && boardList[7] !== 'none') ||
                 (boardList[2] === boardList[5] && boardList[5] === boardList[8] && boardList[8] !== 'none') ||
                 (boardList[0] === boardList[4] && boardList[4] === boardList[8] && boardList[8] !== 'none') ||
-                (boardList[2] === boardList[4] && boardList[4] === boardList[6] && boardList[6] !== 'none') );
+                (boardList[2] === boardList[4] && boardList[4] === boardList[6] && boardList[6] !== 'none') ){
+            
+            victoryDiv.textContent = `The Winner is ${currentPlayer.name}`;
+        }
     }
 
     return {draw, reset, validate};
 }
 
-let bb = boardManager();
+let Player = (a_name)=>{
+    let name = a_name;
+    
+    let swap=()=>{
+        currentPlayer = currentPlayer===player1 ? player2:player1;
+    }
+    let setInit=()=>{
+        currentPlayer = (Math.floor(Math.random() * 11)%2 === 0)? player1:player2;
+    }
+    return {name, swap, setInit}
+}
+
+let player1 = Player('Deepak');
+let player2 = Player('Rudra');
+player1.setInit();
+
+let bb = BoardManager();
 
 bb.draw();
 
